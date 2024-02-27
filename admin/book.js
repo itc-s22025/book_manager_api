504662f9b53ed60c174b2361c8676d0b9addc980
s22025 <s22025@std.it-college.ac.jp>
2024-02-27 17:05:29 +0900

    とりあえず書籍情報登録できた

--- admin/book.js
+++ admin/book.js
@@ -4,7 +4,7 @@ import {PrismaClient} from "@prisma/client";
 const router = express.Router();
 const prisma = new PrismaClient()
 
-//ログインチェック
+//ログインチェック 管理者ユーザならアクセスできる
 router.use((req, res, next) => {
     if (req.user && req.user.isAdmin === true) {
         next();
@@ -15,6 +15,27 @@ router.use((req, res, next) => {
 
 router.get("/", async (req, res, next)=> {
     res.json({msg: "admin/book"})
+})
+
+
+//書籍情報登録
+router.post('/create', async (req, res, next) => {
+    try {
+        const {isbn13, title, author, publishDate} = req.body;
+        const register = await prisma.books.create({
+            data:{
+                isbn13,
+                title,
+                author,
+                publishDate
+            }
+        })
+        res.status(200).json({message: "OK"})
+
+    }catch (e) {
+        console.log(e)
+        res.status(400).json({message: "NG"})
+    }
 })
 
 export default router;
