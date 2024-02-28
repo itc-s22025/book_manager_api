68e35fe8b8a68d00a0e1178cf30668d39188fec0
s22025 <s22025@std.it-college.ac.jp>
2024-02-28 11:28:42 +0900

    全ユーザの貸出中書籍一覧できたっぽい

---
+++ admin/rental.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import {PrismaClient} from "@prisma/client";
+
+const router = express.Router();
+const prisma = new PrismaClient()
+
+
+//ログインチェック 管理者ユーザならアクセスできる
+router.use((req, res, next) => {
+    if (req.user && req.user.isAdmin === true) {
+        next();
+    } else {
+        res.status(403).json({message: "NG"});
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    res.json({msg: "admin/rental"})
+});
+
+
+//全ユーザの貸出中書籍一覧
+router.get('/current', async (req, res, next) => {
+    try {
+        const rentalData = await prisma.rental.findMany({
+            where:{
+                returnDate: null
+            },
+            include:{
+                user:{
+                    select:{
+                        name: true
+                    }
+                },
+                book:{
+                    select:{
+                        title: true
+                    }
+                }
+            },
+            orderBy:{
+                rentalDate: 'desc'
+            }
+        })
+
+        const rentalBooks = rentalData.map(rental => ({
+            rentalId: rental.id,
+            userId: rental.userId,
+            userName: rental.user.name,
+            bookId: rental.bookId,
+            bookName: rental.book.title,
+            rentalDate: rental.rentalDate,
+            returnDeadLine: rental.returnDeadLine
+        }))
+        res.status(200).json({rentalBooks})
+
+    }catch (e) {
+        console.log(e)
+    }
+})
+
+export default router;
